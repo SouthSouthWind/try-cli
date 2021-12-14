3866c51f26229d9d5ce2514cddf690ba25939e8b
SouthSouthWind <know_orn@163.com>
2021-12-15 01:08:40 +0800

    try

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(111);
+#!/usr/bin/env node
+// var co = require('co');
+// var prompt = require('co-prompt');
+var program = require("commander");
+program
+  .option("-d, --debug", "output extra debugging")
+  .option("-s, --small", "small pizza size")
+  .option("-p, --pizza-type <type>", "flavour of pizza");
+
+program.parse(process.argv);
+const options = program.opts();
+if (options.debug) console.log(options);
+console.log("pizza details:");
+if (options.small) console.log("- small pizza size");
+if (options.pizzaType) console.log(`- ${options.pizzaType}`);
